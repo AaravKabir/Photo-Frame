05fbca1e49dac3530378a7932e1fced90c0a12cd
AaravKabir <75346593+AaravKabir@users.noreply.github.com>
2022-04-03 13:29:38 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+function setup(){
+    canvas=createCanvas(680,420);
+    canvas.position(280,270);
+    photo="https://images.pexels.com/photos/189349/pexels-photo-189349.jpeg"
+    
+    }
+    function preload(){
+    
+    }
+    
+    function draw(){
+        
+        rect(25, 40, 40, 320);
+        rect(580, 40, 40, 320);
+        rect(25, 25, 580, 40);
+        rect(25, 360, 580, 40);
+        fill(0,0,200);
+
+        circle(45, 45, 50,);
+        circle(600, 45, 50,);
+        circle(45, 370, 50,);
+        circle(600, 370, 50,);
+        fill(200,0,0);
+        
+
+
+        
+        
+       
+    }
+     
+  
